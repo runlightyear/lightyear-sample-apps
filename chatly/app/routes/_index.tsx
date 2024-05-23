ce0c76613cca017af1d753cb4fe0f0231b116169
Eric Bouck <eric.bouck@gmail.com>
2024-05-23 09:50:44 -0700

    Chatly home screen redirects to messages

--- chatly/app/routes/_index.tsx
+++ chatly/app/routes/_index.tsx
@@ -1,24 +1,6 @@
-import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
-import { requireUserId } from "~/session.server";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node"; // or cloudflare/deno
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  await requireUserId(request);
-
-  return json({});
-}
-
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      Testing
-    </div>
-  );
+  return redirect("/messages");
 }
